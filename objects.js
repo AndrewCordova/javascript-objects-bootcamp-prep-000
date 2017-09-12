7397d6700e7497de6ab8fc2cc76fbf3481a3a084
Andrew Cordova <andrewcordova13@yahoo.com>
2017-09-12 02:09:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,6 @@
-var playlist = new Object();
+var playlist = {
+  artistName: 'songTitle'
+}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, artistName, songTitle)
